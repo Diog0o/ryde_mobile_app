eddb42a31e9fc97c374bca1f510052ed22213db6
Diogo Manuel Lourenço Alcântara Guerreiro <diogo.l.guerreiro@tecnico.ulisboa.pt>
2025-03-11 22:12:51 +0000

    Fix User api route

--- app/(api)/user+api.ts
+++ app/(api)/user+api.ts
@@ -1,36 +1,38 @@
-import { clerk } from '@clerk/clerk-expo/dist/provider/singleton';
 import { neon } from '@neondatabase/serverless';
 
-
-export async function Post (request: Request) {
+export async function POST(request: Request) {
+    console.log("Api user");
     try {
-    const sql = neon(`${process.env.DATABASE_URL}`);
-    const { name, email, clerkId} = await request.json();
+        const sql = neon(`${process.env.DATABASE_URL}`);
+        console.log(process.env.DATABASE_URL);
+        const { name, email, clerkId} = await request.json();
+        console.log(name, email, clerkId);
 
-    if (!name || !email || !clerkId) {
-        return new Response('Missing required fields', {status: 400});
-    }
+        if (!name || !email || !clerkId) {
+            return new Response('Missing required fields', {status: 400});
+        }
+        console.log("Aqui", name, email, clerkId);
 
-    const response = await sql`
-    INSERT INTO users (
-        name, 
-        email,
-        clerk_id
-    )
-    VALUES (
-        ${name}
-        ${email}
-        ${clerkId}
+        const response = await sql`
+        INSERT INTO users (
+            name, 
+            email,
+            clerk_id
         )
-        `;
+        VALUES (
+            ${name},
+            ${email},
+            ${clerkId}
+            )
+            `;
+        console.log(response);
         return new Response(JSON.stringify({data: response}), {status: 201});
 
-            
-        } catch (error) {
-            console.log(error);
-            return new Response('Internal server error', {status: 500});
-        }
-
-
+    
+        
+    } catch (error) {
+        console.log(error);
+        return new Response('Internal server error', {status: 500});
+    }
 }
 
