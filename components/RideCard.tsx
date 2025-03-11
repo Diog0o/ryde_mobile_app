028c91dd10ea5384d145a8874c2f0d9756247949
Diogo Manuel Lourenço Alcântara Guerreiro <diogo.l.guerreiro@tecnico.ulisboa.pt>
2025-03-11 22:23:38 +0000

    Feat: Add RideCard Component

--- components/RideCard.tsx
+++ components/RideCard.tsx
@@ -1,6 +1,7 @@
 import { icons } from "@/constants";
 import { Ride } from "@/types/type";
 import { View, Text, Image } from "react-native";
+import { formatDate, formatTime } from "@/lib/utils";
 
 const RideCard = ({
   ride: {
@@ -18,7 +19,7 @@ const RideCard = ({
 }) => {
   return (
     <View className="flex flex-row items-center justify-center bg-white rounded-lg shadow-sm shadow-neutral-300 mb-3">
-      <View className="flex flex-row items-center justify-between p-3">
+      <View className="flex flex-col items-center justify-center p-3">
         <View className="flex flex-row items-center justify-between">
           <Image
             source={{
@@ -47,9 +48,34 @@ const RideCard = ({
                     Date & Time
                 </Text>
                 <Text className="text-md font-JakartaMedium text-gray-500">
-                    {formatDate(created_at)} {formatDate(ride_time)}
+                    {formatDate(created_at)}, {formatTime(ride_time)}
                 </Text>
             </View>
+            <View className="flex flex-row items-center w-full justify-between mb-5">
+                <Text className="text-md font-JakartaMedium text-gray-500">
+                    Driver
+                </Text>
+                <Text className="text-md font-JakartaMedium text-gray-500">
+                    {driver.first_name} {driver.last_name}
+                </Text>
+            </View>
+            <View className="flex flex-row items-center w-full justify-between mb-5">
+                <Text className="text-md font-JakartaMedium text-gray-500">
+                   Car Seats
+                </Text>
+                <Text className="text-md font-JakartaMedium text-gray-500">
+                    {driver.car_seats}
+                </Text>
+            </View>
+            <View className="flex flex-row items-center w-full justify-between mb-5">
+                <Text className="text-md font-JakartaMedium text-gray-500">
+                   Payment Status
+                </Text>
+                <Text className={`text-md font-JakartaMedium capitalize text-gray-500 ${payment_status === 'paid' ? 'text-green-500' : 'text-red-500'}`}>
+                    {payment_status}
+                </Text>
+            </View>
+
         </View>
       </View>
     </View>
