8b6390846b7f84e6a129ada8c0ddd3fae143f4b5
Diogo Manuel Lourenço Alcântara Guerreiro <diogo.l.guerreiro@tecnico.ulisboa.pt>
2025-03-18 21:06:48 +0000

    Fix: create payment api

--- app/(api)/(stripe)/create+api.ts
+++ app/(api)/(stripe)/create+api.ts
@@ -1,45 +1,53 @@
-import { Stripe } from 'stripe';
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!)
+import { Stripe } from "stripe";
 
-export async function POST( request: Request) {
-    const body = await request.json();
-    const { name, email, amount } = body;
-    if (!name || !email || !amount) {
-        return new Response(JSON.stringify({
-            error: 'Missing required fields',
-            status: 400
-        }))
-    }
-    let customer;
-    const doescostumerExist = await stripe.customers.list({ email: email });
-    if (doescostumerExist) {
-        customer = doescostumerExist.data[0];
-    } else {
-        const newCostumer = await stripe.customers.create({
-            name, 
-            email
-        })
-        customer = newCostumer;
-    }
-    const ephemeralKey = await stripe.ephemeralKeys.create(
-        {customer: customer.id},
-        {apiVersion: '2025-02-24.acacia'}
-      );
-      const paymentIntent = await stripe.paymentIntents.create({
-        amount: parseInt(amount) * 100,
-        currency: 'usd',
-        customer: customer.id,
-        automatic_payment_methods: {
-          enabled: true,
-          allow_redirects: 'never'
-        },
-      });
-    
-      return new Response(
-        JSON.stringify({
-            paymentIntent: paymentIntent,
-            ephemeralKey: ephemeralKey,
-            customer: customer.id,
-      })
-    );
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+
+export async function POST(request: Request) {
+  const body = await request.json();
+  const { name, email, amount } = body;
+
+  if (!name || !email || !amount) {
+    return new Response(JSON.stringify({ error: "Missing required fields" }), {
+      status: 400,
+    });
+  }
+
+  let customer;
+  const doesCustomerExist = await stripe.customers.list({
+    email,
+  });
+
+  if (doesCustomerExist.data.length > 0) {
+    customer = doesCustomerExist.data[0];
+  } else {
+    const newCustomer = await stripe.customers.create({
+      name,
+      email,
+    });
+
+    customer = newCustomer;
+  }
+
+  const ephemeralKey = await stripe.ephemeralKeys.create(
+    { customer: customer.id },
+    { apiVersion: "2024-06-20" },
+  );
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: parseInt(amount) * 100,
+    currency: "usd",
+    customer: customer.id,
+    automatic_payment_methods: {
+      enabled: true,
+      allow_redirects: "never",
+    },
+  });
+
+  return new Response(
+    JSON.stringify({
+      paymentIntent: paymentIntent,
+      ephemeralKey: ephemeralKey,
+      customer: customer.id,
+    }),
+  );
 }
