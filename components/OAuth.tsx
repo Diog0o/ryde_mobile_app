a9e25879f68cc96e08dd3a32bcdcd671e73970c7
Diogo Manuel Lourenço Alcântara Guerreiro <diogo.l.guerreiro@tecnico.ulisboa.pt>
2025-03-22 15:03:28 +0000

    Feat: OAuth component

--- components/OAuth.tsx
+++ components/OAuth.tsx
@@ -1,9 +1,26 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, Alert } from "react-native";
 import CustomButton from "./customButton";
 import { icons } from "@/constants";
+import { useOAuth, useSSO } from "@clerk/clerk-expo";
+import { googleOAuth } from "@/lib/auth";
+import { router } from "expo-router";
 
 const OAuth = () => {
-  const handleGoogleSignIn = async () => {};
+  
+  const { startSSOFlow } = useSSO();
+
+  const handleGoogleSignIn = async () => {
+    const result = await googleOAuth(startSSOFlow);
+
+    if (result.code === "session_exists") {
+      Alert.alert("Success", "Session exists. Redirecting to home screen.");
+      router.replace("/(root)/(tabs)/home");
+    }
+
+    Alert.alert(result.success ? "Success" : "Error", result.message);
+  };
+
+
   return (
     <View>
       <View className="flex flex-row justify-center items-center mt-4">
